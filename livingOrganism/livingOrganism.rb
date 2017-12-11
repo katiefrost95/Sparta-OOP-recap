e56c92e7e3f133527f6eaaa1360bc5a809bd8d54
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-12-11 12:19:07 +0000

    new method

---
+++ livingOrganism/livingOrganism.rb
@@ -0,0 +1,14 @@
+class LivingOrganism
+
+  # @ Instance Variable @@ is class variable
+  @@number_of_living_organism = 0
+
+  def self.alive
+    print 'Living organisms move, feed, respire (burn food to make energy), grow, sense their environment, excrete and reproduce. These seven characteristics are sometimes summarized by the words "MRS GREN". functions of: Movement, Respiration, Sensitivity, Growth, Reproduction,Excretion, Nutrition'
+  end
+
+end
+
+#Have to make an instance of a method before it will work
+# organism = LivingOrganism.new
+LivingOrganism.alive
